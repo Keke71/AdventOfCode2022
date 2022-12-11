a2118e1dc774d0e6b0f0c2adc3db14e5edecede0
Markus <mkreller@live.de>
2022-12-11 02:42:50 +0100

    Day 10

---
+++ Day10-01.py
@@ -0,0 +1,43 @@
+from ast import operator
+from helpers.problemrunner import run_problem
+
+
+@run_problem
+def run():
+    with open("Day10.txt") as f:
+        operations = list(filter(None, (line.rstrip() for line in f)))
+
+        parser = SignalParser()
+        parser.perform_operations(operations)
+
+        return parser.signal_strength
+
+
+class SignalParser:
+    cycles_of_interest = [20 + i * 40 for i in range(6)]
+
+    def __init__(self):
+        self.signal_strength = 0
+        self.x = 1
+        self.cycle = 0
+
+    def perform_operations(self, operations):
+        for operation in operations:
+            self.cycle += 1
+            self.check_cycle()
+            if operation.startswith("addx"):
+                self.cycle += 1
+                self.check_cycle()
+                self.x += int(operation.split()[1])
+            if len(self.cycles_of_interest) == 0:
+                break
+        return sum
+
+
+    def check_cycle(self):
+        if self.cycle == self.cycles_of_interest[0]:
+            self.signal_strength += self.x * self.cycle
+            self.cycles_of_interest = self.cycles_of_interest[1:]
+                    
+
+run()
---
+++ Day10-02.py
@@ -0,0 +1,46 @@
+from ast import operator
+from helpers.problemrunner import run_problem
+
+
+@run_problem
+def run():
+    with open("Day10.txt") as f:
+        operations = list(filter(None, (line.rstrip() for line in f)))
+
+        parser = SignalParser()
+        parser.perform_operations(operations)
+
+        return '\n' + '\n'.join(''.join(c for c in line) for line in parser.output)
+
+
+class SignalParser:
+    
+    width = 40
+    height = 6
+
+    def __init__(self):
+        self.x = 1
+        self.cycle = 0
+        self.current_line = 0
+        self.output = [[' '] * self.width for _ in range(self.height)]
+
+
+    def perform_operations(self, operations):
+        for operation in operations:
+            self.cycle += 1
+            self.current_line = self.cycle // self.width
+            self.draw_pixel()
+            if operation.startswith("addx"):
+                self.cycle += 1
+                self.draw_pixel()
+                self.x += int(operation.split()[1])
+        return sum
+
+
+    def draw_pixel(self):
+        position_in_line = self.cycle % self.width - 1;
+        if position_in_line >= self.x - 1 and position_in_line <= self.x + 1:
+            self.output[self.current_line][position_in_line] = "#"
+                    
+
+run()
